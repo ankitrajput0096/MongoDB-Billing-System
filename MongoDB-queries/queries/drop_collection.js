b9c739cff5f2b429ff41487475f0395c8d9da716
Ankit Rajput <ankitrajput@Ankits-MacBook-Pro.local>
2025-08-25 12:52:35 -0700

    adding drop commands

---
+++ MongoDB-queries/queries/drop_collection.js
@@ -0,0 +1,4 @@
+// drop_collection.js
+// Drop the audit_logs collection from the billing database
+db.audit_logs.drop();
+// This removes the entire audit_logs collection and its documents
---
+++ MongoDB-queries/queries/drop_database.js
@@ -0,0 +1,5 @@
+// drop_database.js
+// Drop the entire billing database (DESTRUCTIVE OPERATION!)
+db.dropDatabase();
+// This removes the entire database including all collections and data
+// WARNING: This operation cannot be undone!
